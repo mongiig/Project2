c05574c65d003da77576dacfbe728ac947e84176
Igor <Igor>
2020-09-18 16:06:51 +0200

    boolean conditions lesson 13

--- script.js
+++ script.js
@@ -23,22 +23,52 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const   a = prompt('Один из последних просмотренных фильмов?',''),
-        b = prompt('На сколько оцените его?',''),
-        c = prompt('Один из последних просмотренных фильмов?',''),
-        d = prompt('На сколько оцените его?','');
+// const   a = prompt('Один из последних просмотренных фильмов?',''),
+//         b = prompt('На сколько оцените его?',''),
+//         c = prompt('Один из последних просмотренных фильмов?',''),
+//         d = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// if (4==4) {
+//     console.log('Ok!');
+// } else {
+//     cons ole.log('Error');
+// };
+
+const num = '50';
+
+// if (num < 49) {
+//     console.log('Error');   
+// } else if (num > 100) {
+//     console.log('Много');    
+// } else {
+//     console.log('ОК!');
+// };
+
+switch (num) {
+    case '49':
+        console.log('Error');
+        break;
+    case '100':
+        console.log('Error2');
+        break;
+    case '50':
+        console.log('Yes!');
+        break;
+    default:
+        break;
+}
